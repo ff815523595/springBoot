f765e6ff2217419ae61780b8f30501bfeae2d9b2
xk <815523595@qq.com>
2017-11-29 13:57:24 +0800

    thymeleaf模板位置设置

--- src/main/java/com/myboot/controller/ThymeleafController.java
+++ src/main/java/com/myboot/controller/ThymeleafController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by a on 2017/11/29.
@@ -22,5 +23,11 @@ public class ThymeleafController {
 
         model.put("xname" , name);
         return "index";
+    }
+    @RequestMapping(value = "thAjax")
+    @ResponseBody
+    public String String(@RequestParam(name = "username",required = false,defaultValue = "jack") String name){
+        logger.info("开始执行thymeleaf的异步ajax方法，获取参数name="+name);
+        return name;
     }
 }
