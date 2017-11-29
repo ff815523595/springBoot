40f4628f8d8c7449ef38266da157613d210dc533
xk <815523595@qq.com>
2017-11-29 15:12:46 +0800

    druid

--- src/main/java/com/myboot/Application.java
+++ src/main/java/com/myboot/Application.java
@@ -1,9 +1,13 @@
 package com.myboot;
 
+import com.myboot.servlet.DruidStatFilter;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
@@ -13,10 +17,12 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ComponentScan("com.myboot")
 @MapperScan(basePackages = "com.myboot.mapper")
+@ServletComponentScan(basePackages = "com.myboot.servlet")
 /*@ImportResource(locations={"classpath:application-bean.xml"}) 可导入指定的xml文件*/
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
